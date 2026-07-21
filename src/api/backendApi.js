0e0cd46bccda2f7cb558842fd25e3f512e82e605
Muhammad Farman <farman4k@gmail.com>
2026-07-21 21:31:33 +0300

    small fixed

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const backendApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+   withCredentials: true,
 });
 
 backendApi.interceptors.request.use((config) => {
